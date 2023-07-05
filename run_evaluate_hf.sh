00f143a34a5414c3e6c02bb3cdc2b0f51f79d5c2
Khalid <49135754+KhalidAlt@users.noreply.github.com>
2023-07-06 00:18:21 +0300

    Update run_evaluate_hf.sh

--- run_evaluate_hf.sh
+++ run_evaluate_hf.sh
@@ -1,7 +1,7 @@
-Model_name="/media/khalid/HDD2/Huggingface_models/m2m_12B"
-tokenizer_name="/media/khalid/HDD2/Huggingface_models/m2m_12B"
+Model_name="/m2m_12B"
+tokenizer_name="/m2m_12B"
 data_path='facebook/flores'
-cache_dir="/media/khalid/data_disk/cache_1time/"
+cache_dir="/cache/"
 subset='eng_Latn-arb_Arab'
 
 CUDA_VISIBLE_DEVICES=1 python ./evalute_hf.py  \
